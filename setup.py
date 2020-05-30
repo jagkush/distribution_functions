5c0d75d68fc911dbe678e97490da6a3d8f7b98dd
kusade gbenga <jagkush2006@gmail.com>
2020-05-30 08:33:39 +0100

    First distribution code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'Distribution',
+    version = '0.1',
+    description = 'Gaussian Distribution',
+    packages = ['Distribution'],
+    author = "Gbenga Kusade",
+    author_email = "jagkush2006@gmail.com",
+    zip_safe = False
+)
